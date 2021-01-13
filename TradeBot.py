d76be8ac6703cc14d6c5580d9d67111fd8c2351d
JackBaynham14 <jack_baynham@outlook.com>
2021-01-13 10:19:05 -0700

    Add loadData() and start new function definitions

--- TradeBot.py
+++ TradeBot.py
@@ -13,6 +13,8 @@ def tradeBot(dataFile, minLength=1, maxLength=100):
     Outputs:
         - string containing a signal if present and information about the used moving averages
     '''
+    # Input validation
+    assert maxLength > minLength
 
     # Load closing price data into array
     data = loadData(dataFile, minLength, maxLength)
@@ -24,9 +26,10 @@ def tradeBot(dataFile, minLength=1, maxLength=100):
     # Iterate through every allowed moving average pair
     for slowLength in range(minLength, maxLength):
         slowLength += 1
+
         # Find values for slow moving average
 
-        for fastLength in range(minLength, maxLength):
+        for fastLength in range(minLength, slowLength):
             # Find values for fast moving average
 
             # Find crossovers
@@ -34,12 +37,56 @@ def tradeBot(dataFile, minLength=1, maxLength=100):
             # Calculate performance
 
             # Update current bestPair/bestPairScore if better
+            pass
     
     # Check for crossover of best pair in most recent period
 
     # Return crossover if present and information about the best pair
+    return
 
 def loadData(dataFile, minLength, maxLength):
     '''
     Load closing price data from file, validate the format, and return an array of closing prices
     '''
+    # Input validation
+    assert os.path.isfile(dataFile)
+    assert maxLength > minLength
+
+    data = []
+
+    with open(dataFile, 'r') as file:
+        lines = file.readlines()
+
+        # Check file format
+        assert lines[0] == 'Date,Open,High,Low,Close,Adj Close,Volume\n'
+
+        # Iterate over each line to check and save closing price
+        for line in lines[1:]:
+            line = line.split(',')
+            data.append(float(line[4]))
+
+    return data
+
+def findMovAvgValues(data, length):
+    '''
+    Finds the values of a moving average
+    '''
+
+    pass
+
+def findCrossovers(fastValues, slowValues):
+    '''
+    Finds crossovers of the fast values crossing the slow values
+    '''
+
+    pass
+
+def findPerformance(data, crossovers):
+    '''
+    Finds average sell and buy prices and returns the difference
+    '''
+
+    pass
+
+#Program testing
+tradeBot('Data/TMHC.csv')
