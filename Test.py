311f7174342e37826918ffe195c0b3f7de055399
JackBaynham14 <jack_baynham@outlook.com>
2021-01-15 09:44:36 -0700

    Initialize test module

---
+++ Test.py
@@ -0,0 +1,13 @@
+'''
+This program tests TradeBoy.py over a period of time
+'''
+
+from TradeBot import tradeBot
+
+# Settings
+accountBalance = 100000
+positionSize = 0.2
+testLength = 120
+shorting = False
+pyrammiding = False
+fileName = 'Data/TMHC.csv'
